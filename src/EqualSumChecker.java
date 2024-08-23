c51a6667d1566a3600f9bdf6b15eef1302f9bb90
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-08-23 13:22:28 -0400

    fbg

--- src/EqualSumChecker.java
+++ src/EqualSumChecker.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class EqualSumChecker {
+public class EqualSumChecker {
+    public static void main(String[] args) {
+        System.out.println(hasEqualSum(1, 1, 1));
+        System.out.println(hasEqualSum(1, 1, 2));
+        System.out.println(hasEqualSum(1, -1, 0));
+    }
+
+    private static boolean hasEqualSum(int num1, int num2, int num3) {
+        int sum = num1 + num2;
+        if (sum == num3) {
+            return true;
+        }else{
+            return false;
+
+    }
+    }
 }
+
